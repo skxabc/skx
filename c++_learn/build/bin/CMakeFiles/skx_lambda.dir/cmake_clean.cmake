51ae03f7551deaa67dedb42a911eb11e61feef47
shikaixun <shikaixun@haier.com>
2021-04-02 15:23:06 +0800

    add c++_learn directory

---
+++ c++_learn/build/bin/CMakeFiles/skx_lambda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skx_lambda.dir/lambda.o"
+  "skx_lambda"
+  "skx_lambda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skx_lambda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
