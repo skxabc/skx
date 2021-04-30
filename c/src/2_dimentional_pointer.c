c534dbbe50a2faa9fd64679ce5ea476740dcb412
skxabc <282612465@qq.com>
2021-04-30 15:09:29 +0800

    test

--- c/src/2_dimentional_pointer.c
+++ c/src/2_dimentional_pointer.c
@@ -14,10 +14,12 @@ void get(sttest *r)
 int main(int argn, char * argv[])
 {
     // sttest *A = (sttest *)malloc(sizeof(sttest));
-    A.a = 1;
-    A.b = 2;
-    sttest *B  = (sttest *)malloc(sizeof(sttest));
-    get(B);
-    printf("B->a:%d B->b:%d\n",B->a,B->b);
+    // A.a = 1;
+    // A.b = 2;
+    // sttest *B  = (sttest *)malloc(sizeof(sttest));
+    // get(B);
+    // printf("B->a:%d B->b:%d\n",B->a,B->b);
+    char *a = "{\"header\":{\"name\":\"resourceDisWakeupResponse\",\"namespace\":\"resourceConfig\"},\"payload\":{\"ratioActive\":0,\"arrayType\":0,\"boardType\":9,\"debug\":1,\"fileNum\":101,\"energyDiff\":0.1,\"micRatio\":6,\"blockSize\":115200,\"pat";
+    printf("strlen(a):%d", strlen(a));
     return 0;
 }
