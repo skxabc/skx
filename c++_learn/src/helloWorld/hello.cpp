5ddc5e6da78f8d8440dda06e3c4d7d6320aaaaef
skxabc <282612465@qq.com>
2022-11-01 22:26:22 +0800

    add hello.cpp for future/promise

--- c++_learn/src/helloWorld/hello.cpp
+++ c++_learn/src/helloWorld/hello.cpp
@@ -4,70 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
-using namespace std;
+
+#include <iostream>
+#include <future>
+#include <chrono>
  
-// void* threadFunc(void* p)  
-// {  
-// 	char szTest[1024 * 32] = {0};
-//     return NULL;  
-// }  
-<<<<<<< HEAD
- 
-int main(void)
-{
-	// // void *ret;
-	// pthread_t id;  
-    // pthread_create(&id, NULL, threadFunc, NULL);  
-	// // pthread_join(id, &ret);
-	// pthread_detach(id);
-	// sleep(1);
-	string str1, str2;
-	while(cin >> str1){
-		str2 += str1 + ' ';
-		if(cin.get() == '\n')break;
-	}
-	cout << "max:" << str2 << endl;
+std::promise<int> promis;
+int main(int argc, const char * argv[]) {
+    std::future<int> fuResult = promis.get_future();
+    std::thread t([](){
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        promis.set_value(123);
+    });
+    t.detach();
+    std::cout<<"detach..."<<std::endl;
+    std::cout<<fuResult.get()<<std::endl;
     return 0;
-=======
-// string make_lu(int cunt, const string &str, const string s="s"){
-// 	return cunt > 1 ? str + s: str;
-// }
-// int f(int, int);
-// std::vector<decltype(f)*> fVec;
-// using f=int(int, int);
-// vector<f*>fVec;
-// usint f=int(*)(int, int);
-// vector<f>fVec;
-struct Sales_data
-{
-	string bookNo;
-	unsigned units_sold = 0;
-	double revenue = 0.0;
-};
-void test701()
-{
-	Sales_data total;
-	if(cin>>total.bookNo>>total.units_sold>>total.revenue){
-		Sales_data trans;
-		while(cin>>trans.bookNo>>trans.units_sold>>trans.revenue){
-			if(total.bookNo == trans.bookNo){
-				total.units_sold += trans.units_sold;
-				total.revenue += trans.revenue;
-			}else{
-				cout<<total.bookNo<<" "<<total.units_sold<<" "<<total.revenue<<endl;
-				total = trans;
-			}
-		}
-		cout<<total.bookNo<<" "<<total.units_sold<<" "<<total.revenue<<endl;
-	}else{
-		cerr<<"No data"<<endl;
-	}
-	return;
-}
-int main(void)
-{
-	test701();
-	return 0;
->>>>>>> d68f195c1ed9cf10faca87eb604d5f9ba6a4eca5
 }
